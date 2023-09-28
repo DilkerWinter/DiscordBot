edfd761ac2ca5ab78df8b98edbea41b0f3e68cfa
DilkerWinter <146080789+DilkerWinter@users.noreply.github.com>
2023-09-28 04:37:07 -0300

    Add files via upload

---
+++ Embed_Text.py
@@ -0,0 +1,26 @@
+ajuda_texto = """
+```✦ "oi": Cumprimenta o usuário.
+✦ "tudo bem?": Pergunta como o usuário está.
+✦ "quem é um gostoso": Diz quem é um gostoso.
+✦ "vem na sincera": O Desafia para uma briga.
+✦ "bot burro": O bot assume que é inútil.
+✦ "nerdola": Mostra um emoji nerd.
+✦ "boa noite": Deseja boa noite ao usuário.
+✦ "pete": Repete a mensagem "repete".
+✦ "barato": Mostra gif do barato.
+✦ "au au": Mostra gif de cachorros.```
+        """
+ajuda_comandos = """
+```✦ "/help": Mostra o menu de ajuda.
+✦ "/random": Gera um número entre 1 e 100.
+✦ "/soun": Responde "Sim!" ou "Não!".
+✦ "/repete: [mensagem]": Repete a mensagem.
+✦ "/moeda": Para Decidir no Cara ou Coroa.
+✦ "/soma": Para executar soma.
+✦ "/sub": Para executar subtração.
+✦ "/vezes": Para executar multiplicação.
+✦ "/div": Para executar divisão.
+✦ "/ping": Mostra o ping atual do bot.
+✦ "/clear": Limpa as mensagens do canal.
+✦ "/desligar": Desliga o Bot. ```
+    """
